f91b904d207f1cec9c8bcf5bc875724a3e6ad286
Karina Escobar <karina.escobar@greenmonks.net>
2022-06-12 12:46:19 -0300

    mongo model  task

---
+++ src/models/Task.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+//Schema son los campos
+//model nombre de la tabla
+//timestamps = true me crea los campos createdAt updatedAt
+// trim:true quita los espacios de la cadena
+//versionKey: false para que no salga el versionado __v
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+//creo mi modelo de dato
+export default model("Task", taskSchema);
